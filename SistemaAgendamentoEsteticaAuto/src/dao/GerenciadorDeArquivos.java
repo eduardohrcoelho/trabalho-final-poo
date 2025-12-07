cd80d5bacaad6eaa5b9fd7b2da3f1be5be79d9aa
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-07 12:58:45 -0300

    [FEAT] Implementando método para sobrescreverq/deletar linha do arquivo

--- SistemaAgendamentoEsteticaAuto/src/dao/GerenciadorDeArquivos.java
+++ SistemaAgendamentoEsteticaAuto/src/dao/GerenciadorDeArquivos.java
@@ -44,13 +44,14 @@ public class GerenciadorDeArquivos {
         try{
             FileReader fr = new FileReader(arquivo);
             BufferedReader br = new BufferedReader(fr);
-
+            
             String linha;
 
             // Enquanto tiver linha para ler (não for nulo)
             while((linha = br.readLine()) != null){
                 linhas.add(linha);
             }
+            br.close();
         }catch(IOException e){
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         }
@@ -58,12 +59,49 @@ public class GerenciadorDeArquivos {
         return linhas;
     }
 
-    public static void main(String[] args) {
-        GerenciadorDeArquivos.salvar("teste.txt", "Eduardo Henrique");
-        List<String> dados = GerenciadorDeArquivos.ler("teste.txt");
+    // Método para sobrescrever/deletar 
+    public static void sobrescrever(String nomeArquivo, List<String> novasLinhas){
+        File arquivo = new File(nomeArquivo);
 
-        for(String s : dados){
-            System.out.println("Lido do arquivo: " + s);
+        try{
+            FileWriter fw = new FileWriter(arquivo, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for(String linha : novasLinhas){
+                bw.write(linha);
+                bw.newLine();
+            }
+            bw.close();
+        }catch(IOException e){
+            System.err.println("Erro ao sobrescrever o arquivo: " + e.getMessage());
         }
+
+    }
+
+    public static void main(String[] args) {
+        String arquivo = "teste.txt";
+
+        // 1. Testando Salvar (Adiciona 3 pessoas)
+        System.out.println("--- Teste 1: Salvando ---");
+        GerenciadorDeArquivos.salvar(arquivo, "ID:1;Nome:Eduardo");
+        GerenciadorDeArquivos.salvar(arquivo, "ID:2;Nome:Mateus");
+        GerenciadorDeArquivos.salvar(arquivo, "ID:3;Nome:Samuel");
+
+        List<String> lista = GerenciadorDeArquivos.ler(arquivo);
+        System.out.println("Lista Atual: " + lista);
+
+        System.out.println("\n--- Teste 2: Deletando Mateus ---");
+        
+        List<String> novaLista = new ArrayList<>();
+        for (String linha : lista) {
+            if (!linha.contains("Mateus")) { // Só mantém quem NÃO é o Mateus
+                novaLista.add(linha);
+            }
+        }
+
+        GerenciadorDeArquivos.sobrescrever(arquivo, novaLista);
+        
+        // Confere se sumiu
+        System.out.println("Lista Após Deletar: " + GerenciadorDeArquivos.ler(arquivo));
     }
 }
