acd79aab1878dd5ecd00be39a531ac5763663ddf
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-07 12:50:04 -0300

    [FEAT] Implementando método ler para ler todos as linhas do arquivo txt

--- SistemaAgendamentoEsteticaAuto/src/dao/GerenciadorDeArquivos.java
+++ SistemaAgendamentoEsteticaAuto/src/dao/GerenciadorDeArquivos.java
@@ -1,9 +1,13 @@
 package dao;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GerenciadorDeArquivos {
@@ -29,7 +33,37 @@ public class GerenciadorDeArquivos {
         
     }
 
+    // Método para ler todas as linhas do arquivo e devolver uma lista de String
+    public static List<String> ler(String nomeArquivo){
+        List<String> linhas = new ArrayList<>();
+        File arquivo = new File(nomeArquivo);
+
+        // Se o arquivo não existe, devolve lista vazia
+        if(!arquivo.exists()){return linhas;}
+
+        try{
+            FileReader fr = new FileReader(arquivo);
+            BufferedReader br = new BufferedReader(fr);
+
+            String linha;
+
+            // Enquanto tiver linha para ler (não for nulo)
+            while((linha = br.readLine()) != null){
+                linhas.add(linha);
+            }
+        }catch(IOException e){
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+        return linhas;
+    }
+
     public static void main(String[] args) {
         GerenciadorDeArquivos.salvar("teste.txt", "Eduardo Henrique");
+        List<String> dados = GerenciadorDeArquivos.ler("teste.txt");
+
+        for(String s : dados){
+            System.out.println("Lido do arquivo: " + s);
+        }
     }
 }
