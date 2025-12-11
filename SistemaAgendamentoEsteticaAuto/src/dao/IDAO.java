b64093659204a81470d1694dde65754ded6a40df
Mateus Von <Mateusvon205@gmail.com>
2025-12-11 19:46:26 -0300

    [REFACTOR] Formatando as classes do projeto.

--- SistemaAgendamentoEsteticaAuto/src/dao/IDAO.java
+++ SistemaAgendamentoEsteticaAuto/src/dao/IDAO.java
@@ -3,21 +3,21 @@ package dao;
 import java.util.List;
 
 // interface generica dao, IDAO, <T> = aceita qualquer classe (Carro, Cliente, Moto, etc)
-public interface IDAO<T>{
+public interface IDAO<T> {
 
-    // Método para salvar um objeto novo no arquivo
-    public boolean salvar(T objeto);
+  // Método para salvar um objeto novo no arquivo
+  public boolean salvar(T objeto);
 
-    // Método para etorna todos os dados do arquivo
-    public List<T> listar();
+  // Método para etorna todos os dados do arquivo
+  public List<T> listar();
 
-    // Método para atualizar um objeto existente
-    public boolean atualizar(T objeto);
+  // Método para atualizar um objeto existente
+  public boolean atualizar(T objeto);
 
-    // Método para remover um objeto pelo ID
-    public boolean deletar(int id);
+  // Método para remover um objeto pelo ID
+  public boolean deletar(int id);
 
-    // Método para buscar objeto específico pelo ID
-    public T buscarPorId(int id);
+  // Método para buscar objeto específico pelo ID
+  public T buscarPorId(int id);
 
 }
--- SistemaAgendamentoEsteticaAuto/src/model/StatusPagamento.java
+++ SistemaAgendamentoEsteticaAuto/src/model/StatusPagamento.java
@@ -1,7 +1,7 @@
 package model;
 
 public enum StatusPagamento {
-    PENDENTE,
-    PAGO,
-    CANCELADO;
+  PENDENTE,
+  PAGO,
+  CANCELADO;
 }
